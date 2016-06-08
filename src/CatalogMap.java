34db2ef9a453f28157dabdbd7917aaaf6bbf2b66
9thScientist <bc.tudo@gmail.com>
2016-06-08 14:24:28 +0000

    CatalogMap now implements Map<K>

--- src/CatalogMap.java
+++ src/CatalogMap.java
@@ -1,5 +1,5 @@
  /**
- * 
+ *
  */
 
 import java.util.*;
@@ -8,9 +8,9 @@ import java.util.List;
 import java.util.TreeSet;
 import java.util.ArrayList;
 
-public class CatalogMap<K,V> {
+public class CatalogMap<K,V> implements Map<K,V> {
     private List<HashMap<K,V>> cat;
-    int size;
+    private int size;
 
     /**
      * Constructs an empty catalog with the specified capacity.
@@ -19,10 +19,10 @@ public class CatalogMap<K,V> {
     public CatalogMap(int size) {
         this.size = size;
         cat = new ArrayList<HashMap<K,V>>(size);
-		
-		for (int i = 0 ; i < size; i++)
-			cat.add(new HashMap<K,V>());
-		
+
+        for (int i = 0 ; i < size; i++)
+            cat.add(new HashMap<K,V>());
+
     }
 
     /**
@@ -31,9 +31,9 @@ public class CatalogMap<K,V> {
     public CatalogMap() {
         size = 0;
         cat = new ArrayList<HashMap<K,V>>();
-		
-		for (int i = 0 ; i < size; i++)
-			cat.add(new HashMap<K,V>());
+
+        for (int i = 0 ; i < size; i++)
+            cat.add(new HashMap<K,V>());
     }
 
     /**
@@ -43,8 +43,8 @@ public class CatalogMap<K,V> {
         size = c.size();
         cat = new ArrayList<HashMap<K,V>>(size);
 
-		for (int i = 0 ; i < size; i++)
-            cat.add(new HashMap<K,V>(c.get(i)));   
+        for (int i = 0 ; i < size; i++)
+            cat.add(new HashMap<K,V>(c.get(i)));
     }
 
     /**
@@ -53,8 +53,8 @@ public class CatalogMap<K,V> {
      * @throws IndexOutOfBoundsException Se o índice não está dentro do catálogo
      */
     public Map<K,V> get(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= size) 
-            throw new IndexOutOfBoundsException(); 
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
 
         return new HashMap<K,V> (cat.get(index));
     }
@@ -67,15 +67,15 @@ public class CatalogMap<K,V> {
      * @throws IndexOutOfBoundsException Se o índice não está dentro do catálogo
      */
     public V get(int index, K key) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= size) 
-            throw new IndexOutOfBoundsException(); 
-            
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
         return cat.get(index).get(key);
     }
-    
+
     /**
      * Associa um valor especificado a uma chave neste mapeamento.
-     * Se a chave indicida já estava mapeada, o valor anterior será 
+     * Se a chave indicida já estava mapeada, o valor anterior será
      * substituido pelo valor especificado.
      * @param index - index at which the specified element is to be inserted
      * @param key Chave com o valor que será associado
@@ -129,33 +129,152 @@ public class CatalogMap<K,V> {
     }
 
     /**
-     * Retorna true se e só se a chave dada estiver a ser mapeada 
+     * Remove todos os mapeamentos deste mapa
+     */
+    public void clear() {
+        for (HashMap hm : cat)
+            hm.clear();
+    }
+
+    /**
+     * Retorna true se e só se a chave dada estiver a ser mapeada
      * no mapeamento do índice dado.
      * (Só pode existir, no máximo, uma destas chaves)
      * @param index índice do mapeamento a procurar
      * @param key Chave cuja presença será testada
      * @return true se a chave dada é mapeada neste mapeamento
-	 */
-	public boolean containsKey(int index, K key) throws IndexOutOfBoundsException{
-		if (index < 0 || index >= size) 
-			throw new IndexOutOfBoundsException(); 
-	
-		return cat.get(index).containsKey(key); 
-	}
+     */
+    public boolean containsKey(int index, K key) throws IndexOutOfBoundsException{
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
 
-	/**
-	 * Retorna true se e só se o valor dado estiver a ser mapeada, 
-	 * pelo menos uma vez, no mapeamento do índice dado. 
-	 * @param index índice
-	 * @param value Chave cuja presença será testada
-	 * @return true se a chave dada é mapeada neste mapeamento
-	 * @throws IndexOutOfBoundsException Caso o índice não se encontre no catálogo
-	 */
-	public boolean containsValue(int index, V value) throws IndexOutOfBoundsException{
-		if (index < 0 || index >= size) 
-			throw new IndexOutOfBoundsException(); 
-	
-		return cat.get(index).containsValue(value); 
-	}
+        return cat.get(index).containsKey(key);
+    }
 
+    /**
+     * Retorna true se e só se o valor dado estiver a ser mapeada,
+     * pelo menos uma vez, no mapeamento do índice dado.
+     * @param index índice
+     * @param value Chave cuja presença será testada
+     * @return true se a chave dada é mapeada neste mapeamento
+     * @throws IndexOutOfBoundsException Caso o índice não se encontre no catálogo
+     */
+    public boolean containsValue(int index, V value) throws IndexOutOfBoundsException{
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
+        return cat.get(index).containsValue(value);
+    }
+
+    /**
+     * Retorna um Set com todos os mapeamentos deste mapa
+     * @return um Set com todos os mapeamentos deste mapa
+     */
+    public Set<Map.Entry<K,V>> entrySet() {
+        Set<Map.Entry<K,V>> set = new TreeSet<>();
+        Set<Map.Entry<K,V>> aux;
+
+        for (HashMap hm : cat) {
+            aux = hm.entrySet();
+
+            for (Map.Entry<K,V> m : aux)
+                set.add(m);
+        }
+
+        return set;
+    }
+
+    /**
+     * Compara um objeto especificado com este mapa
+     * @return true caso os objetos sejam iguais,
+     * false caso contrário
+     */
+     public boolean equals(Object o) {
+        if (this == o) return true;
+        if ( o == null || o.getClass() != this.getClass()) return false;
+
+        CatalogMap<K,V> cat = (CatalogMap<K,V>) o;
+
+        if (cat.size() != this.size()) return false;
+
+        for (int i = 0; i < size; i++)
+            if (this.get(i) != cat.get(i)) return false;
+
+        return true;
+     }
+
+    public int hashCode() {
+        int hash = 7;
+
+        hash = 31*hash + size;
+        hash = 31*hash + cat.hashCode();
+
+        return hash;
+    }
+
+    public Set<K> keySet() {
+        Set<K> ret = new TreeSet<>();
+        Set<K> aux;
+
+        for (HashMap hm : cat) {
+            aux = hm.keySet();
+
+            for (K m : aux)
+                ret.add(m);
+        }
+
+        return ret;
+    }
+
+    public boolean containsKey(Object key) {
+
+        for (int i = 0 ; i < size; i++)
+            if (cat.get(i).containsKey(key)) return true;
+
+        return false;
+    }
+
+    public boolean containsValue(Object val) {
+
+        for (int i = 0; i < size; i++)
+            if (cat.get(i).containsValue(val)) return true;
+
+        return false;
+    }
+
+    public V get(Object k) {
+        return null;
+    }
+
+    public V put(K key, V value) {
+        return null;
+    }
+
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    public V remove(Object key) {
+        K chave = (K) key;
+        Object r = null;
+
+        for (HashMap hm : cat)
+            r = hm.remove(chave);
+
+        return (V) r;
+    }
+
+    public Collection<V> values() {
+        Collection<V> cols = new TreeSet<>();
+        Collection<V> aux;
+
+        for(HashMap hm : cat) {
+            aux = hm.values();
+
+            for (V val : aux)
+                cols.add(val);
+        }
+
+        return cols;
+    }
 }
