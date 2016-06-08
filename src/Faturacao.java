ef4ef261d39cbce4db9c019236b1dda54a1fb76c
Adriana Guedes <122@Adriana-2.local>
2016-06-08 02:43:47 +0100

    Adicionados métodos na classe Faturacao

--- src/Faturacao.java
+++ src/Faturacao.java
@@ -2,6 +2,7 @@ import java.lang.String;
 import java.lang.Object;
 import java.util.*;
 import java.lang.*;
+import java.io.Serializable;
 
 
 /**
@@ -10,7 +11,7 @@ import java.lang.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Faturacao
+public class Faturacao implements Serializable
 {
     /**
      * String = produto, Boolean = comprado? Ordenado por ordem alfabetica
@@ -43,6 +44,19 @@ public class Faturacao
     public Faturacao(CatalogMap<String, Boolean> prod,int filiais){
         this.produtos=prod;
         this.filiais=filiais;
+        
+    }
+    
+    
+    public Faturacao(CatalogMap<String, Boolean> prod,int filiais, int prodsComprados, int vendasZero, int[] numCompras, double[][]fat, int[][]quant){
+        this.produtos=prod;
+        this.filiais=filiais;
+        this.produtosComprados= prodsComprados;
+        this.vendasZero= vendasZero;
+        this.numeroCompras=numCompras;
+        this.fat=fat;
+        this.quant=quant;
+        
     }
     
     public Faturacao(Faturacao c){
@@ -83,10 +97,6 @@ public class Faturacao
         return this.quant;
     }
     
-    public double [] getFaturacaoMes(int mes) {
-        return null;
-    }
-    
     
     public void addSale(Venda v){
         this.fat[v.getMes()][v.getFilial()] = (v.getPreco()) * (v.getUnidades());
@@ -100,6 +110,77 @@ public class Faturacao
     public int getNumCompras(int mes) {
         return numeroCompras[mes];
     }
+    
+    
+    public double getFaturacaoMes(int mes) {
+        double soma= 0;
+        for(int i = 0; i < this.filiais; i++) {
+            soma += fat[mes][i];
+        }
+        return soma;
+        
+    }
+    
+    public int getQuantidadeMes(int mes) {
+        int soma = 0;
+        for(int i=0; i<this.filiais;i++) {
+           soma += quant[mes][i];
+        }
+        return soma;
+        
+    }
+    
+    public double getFaturacaoFilial(int filial) {
+        double soma = 0;
+        for (int m = 0; m < 12; m++) {
+            soma += fat[m][filial];
+        }
+        return soma;
+    }
+    
+    public int getQuantidadeFilial(int filial) {
+        int soma = 0 ;
+        for(int m=0; m<12; m++) {
+            soma += quant[m][filial];
+        }
+        
+        return soma;
+    }
+    
+    public double getFaturacao(int mes, int filial){
+        return fat[mes][filial];
+    }
+    
+    
+    public int getQuantidade(int mes,int filial){
+        return quant[mes][filial];
+    }
+    
+    public double getFaturacaoTotal(){
+        double soma= 0;
+        for(int f = 0 ; f < filiais ; f++){
+            for (int m = 0 ; m < 12 ; m++) 
+                soma += fat [m][f];
+            }
+            return soma;
+        
+    }
+    
+    public int getQuantidadeTotal(){
+        int soma= 0;
+        for(int f = 0 ; f < filiais ; f++){
+            for (int m = 0 ; m < 12 ; m++) 
+                soma += quant [m][f];
+            }
+            return soma;
+        }
+        
+        
+        /**
+         * public CatalogMap<String, Boolean> getListaComprados() {}
+         */
+    
+    
     
     public void addProduto(String nome){
         this.produtos.put(nome.charAt(0)-'A' , nome , false);
@@ -116,7 +197,7 @@ public class Faturacao
         if(o==null || o.getClass() != this.getClass()) {
             return false;
         }
-       	Faturacao f = (Faturacao) o;
+        Faturacao f = (Faturacao) o;
         return f.getProdutos().equals(produtos) 
                 && f.getProdutosComprados()== this.produtosComprados
                 && f.getFiliais() == this.filiais
@@ -125,6 +206,8 @@ public class Faturacao
                 && f.getFat().equals(fat)
                 && f.getQuant().equals(quant);
     }
+    
+    
     /**public int hashCode() {
         int hash = 7;
 
@@ -137,5 +220,8 @@ public class Faturacao
         return hash;
     }*/
 }
+
+
+
 
 
