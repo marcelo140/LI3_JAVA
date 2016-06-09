483875041ace07ba8e5039de270974fee0be2b76
9thScientist <bc.tudo@gmail.com>
2016-06-09 11:15:22 +0000

    VendasFilial on its way...

--- src/VendasFilial.java
+++ src/VendasFilial.java
@@ -1,29 +1,34 @@
 
 /**
- * Classe que implementa Vendas por filial. Contém um conjunto de clientes, 
+ * Classe que implementa Vendas por filial. Contém um conjunto de clientes,
  * para cada cliente a sua quantidade mensal e cada produto que o qual comprou
- * 
+ *
  */
 public class VendasFilial {
 
+    private final int MESES = 12;
+    private int numClientes;
+    private int[] numClientesMes;
     private CatalogMap<String, Client> clientes;
 	private CatalogMap<String, Product> produtos;
 
 	/**
-     * Construtor padrão 
+     * Construtor padrão
      */
     public VendasFilial() {
-		clientes = new CatalogMap<String, Client>(12);
-		produtos = new CatalogMap<String, Product>(12);
+        numClientes = 0;
+        numClientesMes = new int[MESES];
+		clientes = new CatalogMap<String, Client>(MESES);
+		produtos = new CatalogMap<String, Product>(MESES);
     }
 
 	/**
 	 * Construtor por parametros
 	 */
-	public VendasFilial(CatalogMap<String, Client> clientes, 
-			CatalogMap<String, Product> produtos) {
-		this.clientes = clientes;
-		this.produtos = produtos;
+	public VendasFilial(int numClientes, int[] numClientesMes,
+                        CatalogMap<String, Client> clientes,
+                        CatalogMap<String, Product> produtos) {
+
 	}
 
 	/**
@@ -31,26 +36,77 @@ public class VendasFilial {
 	 * NÃO FUNCIONA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	 */
 	public VendasFilial(VendasFilial v) {
+
+        numClientes = v.getNumClientes();
+        numClientesMes = v.getNumClientesMes();
 		clientes = v.getClientes();
-		produtos = v.getProdutos();	
+		produtos = v.getProdutos();
 	}
 
-	/**
+    /**
+     * Retorna o número de clientes que compraram nesta filial.
+     * @return o número de clientes que compraram nesta filial
+     */
+    public int getNumClientes() {
+         return numClientes;
+    }
+
+    /**
+      * Retorna um array com o número de clientes que compraram em cada mês.
+      * @return um array com o número de clientes que compraram em cada mês
+      */
+    public int[] getNumClientesMes() {
+        return numClientesMes;
+    }
+
+    /**
+     * Retorna o número de clientes que compraram no mês dado.
+     * @param mes mês
+     * @return o número de clientes que compraram no mês dado
+     */
+    public int getNumClientesMes(int mes) throws InvalidMonthException {
+        if (mes < 0 || mes > 11)
+            throw new InvalidMonthException("Mês inválido.");
+
+       return numClientesMes[mes];
+   }
+
+    /**
 	 * Retorna um CatalogMap que mapeia para cada Cliente, a quantidade que
 	 * comprou e, para cada produto, a quantidade comprada e o total gasto.
 	 */
 	private CatalogMap<String, Client> getClientes() {
-		return clientes;
+		return new CatalogMap<>(clientes);
 	}
 
 	/**
-	 * Retorna um CatalogMap que mapeia para cada Produto, a quantidade 
+	 * Retorna um CatalogMap que mapeia para cada Produto, a quantidade
 	 * total vendida e total faturado, e, para cada cliente, o tipo de
 	 * promoção a que este o comprou.
 	 */
 	private CatalogMap<String, Product> getProdutos() {
-		return produtos;
+		return new CatalogMap<>(produtos);
 	}
+
+    /**
+     * Retorna um Client com todas as propriedas associadas a um cliente,
+     * ou null caso não haja nenhum Client associado ao cliente dado.
+     * @param cliente Cliente cujo o Client associado será retornado
+     * @return um Client com todas as propriedas associadas a um cliente
+     */
+    public Client getCliente(String cliente) {
+        return new Client(clientes.get(cliente));
+    }
+
+    /**
+     * Retorna um Product com todas as propriedas associadas a um produto,
+     * ou null caso não haja nenhum Product associado ao produto dado.
+     * @param produto produto cujo o Product associado será retornado
+     * @return um Product com todas as propriedas associadas a um produto
+     */
+    public Product getProduct(String produto) {
+        return new Product(produtos.get(produto));
+    }
 
 	/**
 	 * Adiciona uma venda a esta estrutura
@@ -64,5 +120,5 @@ public class VendasFilial {
 		produtos.get(mes, produto).add(v);
 		clientes.get(mes, cliente).add(v); */
 	}
-	
+
 }
